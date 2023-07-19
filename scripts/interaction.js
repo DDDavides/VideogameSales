0f1f2f37c1aab993763f2a935a748c70904371f1
Davide Molitierno <davidmolitiern@hotmail.it>
2023-07-19 15:50:11 +0200

    migliorie al codice

--- scripts/interaction.js
+++ scripts/interaction.js
@@ -1,49 +1,49 @@
 // function that closes menu
 function closeMenu() {
-  var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
-} 
+  let dropdowns = document.getElementsByClassName("dropdown-content");
+  let i;
+  for (i = 0; i < dropdowns.length; i++) {
+    let openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
 
-let genreToggled = new Set();
-let platformToggled = new Set();
-let yearToggled = new Set();
+var genreToggled = new Set();
+var platformToggled = new Set();
+var yearToggled = new Set();
 
-let genres = new Set();
-let platforms = new Set();
-let years = new Set();
+var genres = new Set();
+var platforms = new Set();
+var years = new Set();
 
-let allData;
+var allData;
 
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function showDropDownToggles(elementId) {
   closeMenu();
-  toggles = document.getElementById(elementId);
+  let toggles = document.getElementById(elementId);
   toggles.classList.toggle("show");
 
   let buttonHeight = document.getElementsByClassName("dropbtn")[0].getBoundingClientRect().height;
   // change position of dropdown if it is too close to the bottom of the screen
-  if(toggles.getBoundingClientRect().bottom >= window.innerHeight) {
+  if (toggles.getBoundingClientRect().bottom >= window.innerHeight) {
     toggles.style.top = "auto";
     toggles.style.bottom = buttonHeight + "px";
   }
   let header = document.getElementById("header");
 
-  if(toggles.getBoundingClientRect().top <= header.getBoundingClientRect().bottom) {
+  if (toggles.getBoundingClientRect().top <= header.getBoundingClientRect().bottom) {
     toggles.style.top = buttonHeight + "px";
     toggles.style.bottom = "auto";
   }
-  
+
   // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (!event.target.matches('.dropbtn') && !event.target.matches('.toggle') && !event.target.matches('.base_toggle')) {
-        closeMenu();
+      closeMenu();
     }
   }
 }
@@ -64,7 +64,7 @@ function displayInteractive(sales) {
       years.add(d.Year);
     }
   });
-  
+
   // sort the genres, platforms and years
   genres = Array.from(genres).sort();
   platforms = Array.from(platforms).sort();
@@ -73,19 +73,19 @@ function displayInteractive(sales) {
   // display the genres, platforms and years in the dropdown menus
   checkboxContainer = document.getElementById("genreDropdown");
   genres.forEach(d => {
-    checkboxContainer.innerHTML += '<div class="checkbox"><input checked=true class="toggle" type="checkbox" id="'+d+'" name="genre" value="'+d+'"><label class="text" for="'+d+'">'+d+'</label><br></div>';
+    checkboxContainer.innerHTML += '<div class="checkbox"><input checked=true class="toggle" type="checkbox" id="' + d + '" name="genre" value="' + d + '"><label class="text" for="' + d + '">' + d + '</label><br></div>';
   });
   genreToggled = new Set(genres);
 
   checkboxContainer = document.getElementById("platformDropdown");
   platforms.forEach(d => {
-    checkboxContainer.innerHTML += '<div class="checkbox"><input checked=true class="toggle" type="checkbox" id="'+d+'" name="platform" value="'+d+'"><label class="text" for="'+d+'">'+d+'</label><br></div>';
+    checkboxContainer.innerHTML += '<div class="checkbox"><input checked=true class="toggle" type="checkbox" id="' + d + '" name="platform" value="' + d + '"><label class="text" for="' + d + '">' + d + '</label><br></div>';
   });
   platformToggled = new Set(platforms);
 
   checkboxContainer = document.getElementById("yearDropdown");
   years.forEach(d => {
-    checkboxContainer.innerHTML += '<div class="checkbox"><input checked=true class="toggle" type="checkbox" id="'+d+'" name="year" value="'+d+'"><label class="text" for="'+d+'">'+d+'</label><br></div>';
+    checkboxContainer.innerHTML += '<div class="checkbox"><input checked=true class="toggle" type="checkbox" id="' + d + '" name="year" value="' + d + '"><label class="text" for="' + d + '">' + d + '</label><br></div>';
   });
   yearToggled = new Set(years);
 
@@ -94,29 +94,13 @@ function displayInteractive(sales) {
   toggles = document.getElementsByClassName("toggle");
   toggles = Array.from(toggles);
   toggles.forEach(d => {
-    d.onclick = function(d) {
+    d.onclick = function (d) {
       toggle = d.target;
       toggle_name = toggle.getAttribute("name");
       if (toggle.checked) {
-        if (toggle_name == "genre") {
-          genreToggled.add(toggle.value);
-        } 
-        if (toggle_name == "platform") {
-          platformToggled.add(toggle.value);
-        } 
-        if (toggle_name == "year") {
-          yearToggled.add(toggle.value);
-        }
+        eval(toggle_name + "Toggled.add(toggle.value)");
       } else {
-        if (toggle_name == "genre") {
-          genreToggled.delete(toggle.value);
-        } 
-        if (toggle_name == "platform") {
-          platformToggled.delete(toggle.value);
-        }
-        if (toggle_name == "year") {
-          yearToggled.delete(toggle.value);
-        }
+        eval(toggle_name + "Toggled.delete(toggle.value)");
       }
       filterData(allData);
     }
@@ -128,78 +112,36 @@ function displayInteractive(sales) {
 function filterData(data) {
   let filteredData = data.filter(d => {
     if (!genreToggled.has(d.Genre)) {
-        return false;
-      }
+      return false;
+    }
     if (!platformToggled.has(d.Platform)) {
-        return false;
-      }
+      return false;
+    }
     if (!yearToggled.has(d.Year)) {
-        return false;
-      }
+      return false;
+    }
     return true;
   });
   updateChoro(filteredData);
 }
 
-function toggleAllYears(toggle) {
+function toggleAll(toggle, toggle_name) {
   toggles = document.getElementsByClassName("toggle");
   toggles = Array.from(toggles);
   if (toggle.checked) {
-    yearToggled = new Set(years);
+    eval(toggle_name + "Toggled = new Set(" + toggle_name + "s)");
     toggles.forEach(d => {
-      if (d.getAttribute("name") == "year") {
+      if (d.getAttribute("name") == toggle_name) {
         d.checked = true;
       }
     });
   } else {
     toggles.forEach(d => {
-      if (d.getAttribute("name") == "year") {
+      if (d.getAttribute("name") == toggle_name) {
         d.checked = false;
       }
     });
-    yearToggled = new Set();
-  }
-  filterData(allData);
-}
-
-function toggleAllGenres(toggle) {
-  toggles = document.getElementsByClassName("toggle");
-  toggles = Array.from(toggles);
-  if (toggle.checked) {
-    genreToggled = new Set(genres);
-    toggles.forEach(d => {
-      if (d.getAttribute("name") == "genre") {
-        d.checked = true;
-      }
-    });
-  } else {
-    toggles.forEach(d => {
-      if (d.getAttribute("name") == "genre") {
-        d.checked = false;
-      }
-    });
-    genreToggled = new Set();
-  }
-  filterData(allData);
-}
-
-function toggleAllPlatforms(toggle) {
-  toggles = document.getElementsByClassName("toggle");
-  toggles = Array.from(toggles);
-  if (toggle.checked) {
-    platformToggled = new Set(platforms);
-    toggles.forEach(d => {
-      if (d.getAttribute("name") == "platform") {
-        d.checked = true;
-      }
-    });
-  } else {
-    toggles.forEach(d => {
-      if (d.getAttribute("name") == "platform") {
-        d.checked = false;
-      }
-    });
-    platformToggled = new Set();
+    eval(toggle_name + "Toggled = new Set()");
   }
   filterData(allData);
 }
